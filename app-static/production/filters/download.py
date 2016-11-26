1cf3052bb1c4cd772580f8e6de013b069885c811
Blokada <hello@blokada.org>
2016-11-26 19:14:34 +0100

    update production files from dev

---
+++ app-static/production/filters/download.py
@@ -0,0 +1,16 @@
+import urllib2
+
+
+lines = [line.rstrip('\n') for line in open('repo.txt')]
+for line in lines[1:]:
+	l = line.split(';')
+	filename = l[1].split('/')[-1]
+	print l[0]
+	try:
+		response = urllib2.urlopen(l[0])
+		content = response.read()
+		output = open(filename, 'w')
+		output.write(content)
+		output.close()
+	except Exception, e:
+		print str(e.reason)
