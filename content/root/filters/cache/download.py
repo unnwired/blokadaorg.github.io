071264c512df681d3b615320ebd9c3d3f6fabb4a
Blokada <hello@blokada.org>
2017-06-07 11:36:41 +0200

    update new filters format to include optional name and comment

--- content/root/filters/cache/download.py
+++ content/root/filters/cache/download.py
@@ -2,8 +2,8 @@ import urllib2
 
 
 lines = [line.rstrip('\n') for line in open('../../../../api/v1/prod/filters')]
-for i in range(0, len(lines), 8):
-	entry = lines[i:i + 8]
+for i in range(0, len(lines), 9):
+	entry = lines[i:i + 9]
 	name = entry[1]
 	url = entry[6]
 	print name
